1f8865d3d06b29832c6b88464aa7227056be97c3
LatinWarrior <luis.f.blanco@gmail.com>
2024-11-09 17:11:42 -0600

    Add Auth and context

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import AuthWrapper from '@/components/AuthWrapper';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,16 +17,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} antialiased`}>
-        <Header />
-        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-          {children}
-        </main>
-        <Footer />
-        {/* Position absolute */}
-        <ToastContainer />
-      </body>
-    </html>
+    <AuthWrapper>
+      <html lang='en'>
+        <body className={`${inter.className} antialiased`}>
+          <Header />
+          <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+            {children}
+          </main>
+          <Footer />
+          {/* Position absolute */}
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthWrapper>
   );
 }
